f14164e07ce087135a1407908c4e6350ecf466df
Rashidnashtar <Rashidmohamadnashtar2002@gmail.com>
2022-09-07 16:10:43 +0300

    adding state for sign in

--- src/pages/sign-in.js
+++ src/pages/sign-in.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 const SignIn = () => {
+  const handleChange = (event) => {
+    if ((event.target.name = "remember"))
+      setSignInCred({ ...signInCred, remember: !signInCred.remember });
+    else
+      setSignInCred({ ...signInCred, [event.target.name]: event.target.value });
+  };
+  const [signInCred, setSignInCred] = useState({
+    email: "",
+    password: "",
+    remember: "true",
+  });
   const navigate = useNavigate();
   return (
     <div className="site-content">
@@ -37,6 +48,8 @@ const SignIn = () => {
                         className="form-controllar"
                         type="email"
                         placeholder="Email Adderss"
+                        value={signInCred.email}
+                        onChange={handleChange}
                       />
                     </div>
                     <div className="form-group">
@@ -46,15 +59,19 @@ const SignIn = () => {
                         className="form-controllar"
                         type="password"
                         placeholder="Password"
+                        value={signInCred.password}
+                        onChange={handleChange}
                       />
                     </div>
                     <div className="remember-and-password">
                       <div className="login-form-remember">
                         <label>
                           <input
+                            name="remember"
                             id="remembermesignin"
                             type="checkbox"
-                            defaultValue=""
+                            onChange={handleChange}
+                            checked={signInCred.remember}
                           />
                           <span>Remember Me</span>
                         </label>
@@ -104,99 +121,6 @@ const SignIn = () => {
                       Register Now
                     </Link>
                   </div>
-                </div>
-              </div>
-              <div className="user-signup-area">
-                <div className="user-left-thumb">
-                  <img src="/assets/images/others/reg-thumb.jpg" alt="thumb" />
-                </div>
-                <div className="form-content">
-                  <h3>Create an Account</h3>
-                  <form
-                    id="signup-form"
-                    className="default-form signup-form"
-                    method="post"
-                  >
-                    <div className="form-group">
-                      <input
-                        id="name"
-                        name="name"
-                        className="form-controllar"
-                        type="text"
-                        placeholder="Username"
-                      />
-                    </div>
-                    <div className="form-group">
-                      <input
-                        id="email-up"
-                        name="email"
-                        className="form-controllar"
-                        type="email"
-                        placeholder="Email Adderss"
-                      />
-                    </div>
-                    <div className="form-group">
-                      <input
-                        id="pass-up"
-                        name="password"
-                        className="form-controllar"
-                        type="password"
-                        placeholder="Password"
-                      />
-                    </div>
-                    <div className="form-group">
-                      <input
-                        id="pass-up-confirm"
-                        name="password"
-                        className="form-controllar"
-                        type="password"
-                        placeholder="Confirm Password"
-                      />
-                    </div>
-                    <div className="login-form-remember">
-                      <label>
-                        <input
-                          id="remembermesignup"
-                          type="checkbox"
-                          defaultValue=""
-                        />
-                        <span>I Agree to the</span>
-                        <a href="#">Privacy Polciy</a>
-                      </label>
-                    </div>
-                    <div className="form-btn-group">
-                      <button type="submit" className="btn btn-default">
-                        register
-                      </button>
-                      <div className="reg-others-midea">
-                        <div className="text">Or Connect With</div>
-                        <div className="midea-icons">
-                          <ul className="social-share">
-                            <li>
-                              <a className="facebook" href="#">
-                                <i className="fa fa-facebook" />
-                              </a>
-                            </li>
-                            <li>
-                              <a className="twitter" href="#">
-                                <i className="fa fa-twitter" />
-                              </a>
-                            </li>
-                            <li>
-                              <a className="google-plus" href="#">
-                                <i className="fa fa-google-plus" />
-                              </a>
-                            </li>
-                            <li>
-                              <a className="linkedin" href="#">
-                                <i className="fa fa-linkedin" />
-                              </a>
-                            </li>
-                          </ul>
-                        </div>
-                      </div>
-                    </div>
-                  </form>
                 </div>
               </div>
             </div>
